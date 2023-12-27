6eb73250cac2dae4c10d9fcdb1260e421bf16158
erik465 <ehalasz06@gmail.com>
2023-12-27 14:31:01 +0100

    Start the project

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+const db_connection = require("./database/connection");
+
+db_connection(process.env.DB_URI, app);
+
+app.use((__, res, _) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    code: 500,
+    status: "Internal Server Error",
+  });
+});
 
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", async (req, res, next) => {});
+
+router.post("/login", async (req, res, next) => {});
+
+router.post("/logout", async (req, res, next) => {});
